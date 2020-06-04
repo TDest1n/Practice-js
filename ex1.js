56dac4e922f6560e094046b3b9db1bb1dfb8cf69
TDest1n <boxman06@yahoo.com>
2020-06-03 19:37:50 -0500

    This is when I am correcting an upload to GitHub.

---
+++ ex1.js
@@ -0,0 +1,10 @@
+var number = prompt("What's your favorite number?");
+
+if (number >= 1000) {
+    console.log("Woah, that's a big number!");
+} else if (number >= 0) {
+    console.log("Thats's a cool number.");
+} else {
+    console.log("Negative numbers?! That's just bananas.");
+     
+}
